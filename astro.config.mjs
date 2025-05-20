ea4aaf09238f2faec0d1a41ebf22beff26bee4dc
emgrey02 <emgrey02@gmail.com>
2025-05-20 15:25:04 -0400

    Add blog, fix styles, delete unused files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,38 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
-import icon from "astro-icon"
+import icon from 'astro-icon'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
+  site: 'https://accessible-astro-starter.incluud.dev',
+  integrations: [mdx(), icon(), compress()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          logger: {
+            warn: () => {},
+          },
+        },
+      },
+    },
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+        '@content': fileURLToPath(new URL('./src/content', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+        '@public': fileURLToPath(new URL('./public', import.meta.url)),
+        '@post-images': fileURLToPath(new URL('./public/posts', import.meta.url)),
+        '@project-images': fileURLToPath(new URL('./public/projects', import.meta.url)),
+      },
+    },
+  },
 })
